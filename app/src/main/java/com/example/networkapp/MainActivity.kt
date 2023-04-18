153305be4d84ecb6f20936a819d8d40493f0ab6b
Karl Morris <karl.morris@temple.edu>
2023-04-18 07:39:59 -0400

    Initial commit

---
+++ app/src/main/java/com/example/networkapp/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.networkapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.squareup.picasso.Picasso
+import org.json.JSONObject
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var requestQueue: RequestQueue
+    lateinit var titleTextView: TextView
+    lateinit var descriptionTextView: TextView
+    lateinit var numberEditText: EditText
+    lateinit var showButton: Button
+    lateinit var comicImageView: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        requestQueue = Volley.newRequestQueue(this)
+
+        titleTextView = findViewById<TextView>(R.id.comicTitleTextView)
+        descriptionTextView = findViewById<TextView>(R.id.comicDescriptionTextView)
+        numberEditText = findViewById<EditText>(R.id.comicNumberEditText)
+        showButton = findViewById<Button>(R.id.showComicButton)
+        comicImageView = findViewById<ImageView>(R.id.comicImageView)
+
+        showButton.setOnClickListener {
+            downloadComic(numberEditText.text.toString())
+        }
+
+    }
+
+    private fun downloadComic (comicId: String) {
+        val url = "https://xkcd.com/$comicId/info.0.json"
+        requestQueue.add (
+            JsonObjectRequest(url, {showComic(it)}, {
+            })
+        )
+    }
+
+    private fun showComic (comicObject: JSONObject) {
+        titleTextView.text = comicObject.getString("title")
+        descriptionTextView.text = comicObject.getString("alt")
+        Picasso.get().load(comicObject.getString("img")).into(comicImageView)
+    }
+
+
+}
